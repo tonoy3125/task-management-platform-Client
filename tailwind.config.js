cc8202bc116f3b20a6c79623ee9f08f895bb1d6e
tonoy3125 <shaifshajedt@gmail.com>
2023-12-21 19:33:11 +0600

    font and colour added in tailwind.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      lightOne: '#7BD3EA',
+      lightTwo: '#A1EEBD',
+      lightThree: '#F6F7C4',
+      lightFour: '#F6D6D6',
+      darkOne: '#092635',
+      darkTwo: '#1B4242',
+      darkThree: '#5C8374',
+      darkFour: '#9EC8B9',
+    },
+    fontFamily: {
+      lora: ['Lora', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
+    },
   },
   plugins: [require("daisyui")],
 }
